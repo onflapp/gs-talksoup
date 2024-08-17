39d0576523defd760edba99174d04d7b3c55d4c2
onflapp <onflapp@yahoo.com>
2024-08-17 21:31:40 +0200

    update bridge.sh

--- Input/BitlbeeBridge/bridge.sh
+++ Input/BitlbeeBridge/bridge.sh
@@ -3,4 +3,4 @@
 CFGD="$HOME/Library/ApplicationSupport/TalkSoup/bitlbee"
 mkdir -p "$CFGD" 2>/dev/null
 
-bitlbee -v -n -d "$CFGD" -P "$CFGD/bitlbee.pid" -c "$CFGD/bitlbee.conf"
+/usr/sbin/bitlbee -v -n -d "$CFGD" -P "$CFGD/bitlbee.pid" -c "$CFGD/bitlbee.conf"
